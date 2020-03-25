536da7e9c85cf69ff9c1b34149fa1fdd23d44ff4
Tvo9Bysinka <61286196+Tvo9Bysinka@users.noreply.github.com>
2020-03-25 14:58:19 +0300

    Update form.php

--- form.php
+++ form.php
@@ -12,13 +12,13 @@
 			</div>
 			<form method="post" action="index.php" name="contract" >
 				<div id="nam">
-					<p>Name:<input maxlength="25" size="40" name="name" placeholder="First name Last Name"></p>
+					<p>Im9:<input maxlength="25" size="40" name="name" placeholder="First name Last Name"></p>
 				</div>
 				<div id="address">
 					<p>Email:<input type="email" name="email" placeholder="email@yandex.ru"></p>
 				</div>
 				<div id="BIRHYEAR">
-				<p>Year of Birth:
+				<p>Data Rojdeni9:
 					<select name="year" >
 						<option value="1998">1998</option>
 						<option value="1999">1999</option>
@@ -30,17 +30,17 @@
 				</p>	
 				</div>
 				
-				<p>Sex:
+				<p>Sexyalniq POL:
 					<input type="radio" value="M" name="sex">Man
 					<input type="radio" value="F" name="sex">Female
 				</p>	
-				<p>Limbs:
+				<p>Conechnosti:
 					<input type="radio" value="1" name="limbs">1
 					<input type="radio" value="2" name="limbs">2
 					<input type="radio" value="3" name="limbs">3
 					<input type="radio" value="4" name="limbs">4
 				</p>
-				<p>	<label for='sverh[]'>Superpowers:</label></br>
+				<p>	<label for='sverh[]'>Supervozmojnosti:</label></br>
 					<select id="sposobnost" name="sverh[]" multiple="multiple" size="6">	
 						<option value="net" >None</option>
 						<option value="godmod">GodMode</option>
@@ -50,8 +50,8 @@
 						<option value="extrasensory">Extrasensory</option>
 					</select>
 				</p>
-					<textarea cols="40" rows="4" name="biography" placeholder="Here is your brief biography..."></textarea>
-					<p>Do you agree that you are selling your soul to the devil?
+					<textarea cols="40" rows="4" name="biography" placeholder="Vasha biografi9..."></textarea>
+					<p>Najmi galochky esli lubish Mamochky:
 					<input type="checkbox" name="consent" value="yes"></p>
 				<input type="submit" value="Отправить">
 			</form>
